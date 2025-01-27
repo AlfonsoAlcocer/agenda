c097a596f3ace359cc512d12410ffe6fbf8c71b9
Manu <basto3107@gmail.com>
2025-01-27 09:22:17 -0600

    adding structure

---
+++ resources/views/horario/horario.blade.php
@@ -0,0 +1,32 @@
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col"></th>
+            <th scope="col">Lunes</th>
+            <th scope="col">Martes</th>
+            <th scope="col">Miercoles</th>
+            <th scope="col">Jueves</th>
+            <th scope="col">Viernes</th>
+        </tr>
+        </thead>
+    <tbody>
+        <tr>
+            <th>7:00 - 7:50</th>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th>7:50 - 8:40</th>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th>8:40 - 8:40</th>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
