4920be9c08555f28052cbde4f96870edee7ef008
xFelipeSCx <trabajotic94@gmail.com>
2025-01-28 06:27:52 -0600

    ingles a español acciones

--- resources/views/solicitude/index.blade.php
+++ resources/views/solicitude/index.blade.php
@@ -18,7 +18,7 @@
 
                              <div class="float-right">
                                 <a href="{{ route('solicitudes.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
+                                  {{ __('Crear nuevo') }}
                                 </a>
                               </div>
                         </div>
@@ -58,11 +58,11 @@
 
                                             <td>
                                                 <form action="{{ route('solicitudes.destroy', $solicitude->id_solicitud) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('solicitudes.show', $solicitude->id_solicitud) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('solicitudes.edit', $solicitude->id_solicitud) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                    <a class="btn btn-sm btn-primary " href="{{ route('solicitudes.show', $solicitude->id_solicitud) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Ver') }}</a>
+                                                    <a class="btn btn-sm btn-success" href="{{ route('solicitudes.edit', $solicitude->id_solicitud) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Editar') }}</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Eliminar') }}</button>
                                                 </form>
                                             </td>
                                         </tr>
